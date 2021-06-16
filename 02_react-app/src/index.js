6e347de8904c4cf985ce08edb4d71493060b097f
cxx3668119 <862704268@qq.com>
2021-06-16 18:05:36 +0800

    day02

--- 02_react-app/src/index.js
+++ 02_react-app/src/index.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom'
+import React from 'react'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import { sum } from './utils'
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+class App extends React.Component {
+  render() {
+    return {
+      < div >
+      <h2>
+      </h2>
+      </div >
+    }
+}
+}
+
+ReactDOM.render(<h2>hello,cxx</h2>, document.getElementById('root'))
