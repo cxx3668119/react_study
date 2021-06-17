7ce3e19a81d020a38b08f1e38048d303c73c7e42
cxx3668119 <862704268@qq.com>
2021-06-17 15:23:00 +0800

    重要概念父子通讯

--- 04_react_component/src/04_组件通信案例/App.js
+++ 04_react_component/src/04_组件通信案例/App.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react'
 import TabControl from './TabControl'
+
 export default class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      currentTitle: '流行',
+      titles: ['流行', '新款', '精选']
+    }
+  }
+
   render() {
+    const { currentTitle, titles } = this.state
+
     return (
       <div>
-        <TabControl />
+        <TabControl itemClick={index => this.itemClick(index)} titles={titles} />
+        <h2 className="tag-container">{currentTitle}</h2>
       </div>
     )
+  }
+  itemClick(index) {
+    this.setState({
+      currentTitle: this.state.titles[index]
+    })
   }
 }
--- 04_react_component/src/04_组件通信案例/TabControl.js
+++ 04_react_component/src/04_组件通信案例/TabControl.js
@@ -1,11 +1,44 @@
 import React, { Component } from 'react'
+// import PropTypes from 'prop-types'
 
 export default class TabControl extends Component {
-  render() {
-    return (
-      <div>
+  constructor(props) {
+    super(props)
+    this.state = {
+      currentIndex: 0,
+    }
+  }
 
+  render() {
+    let { titles } = this.props
+    let { currentIndex } = this.state
+    return (
+      <div className="tab-control">
+        {
+          titles.map((item, index) => {
+            return (
+              <div key={index}
+                className={" tab-item " + (index === currentIndex ? "active" : "")}
+                onClick={e => this.itemClick(index)}>
+                <span className={index === currentIndex ? "tab-span" : ""}>{item}</span>
+              </div>
+
+            )
+          })
+        }
       </div>
     )
   }
+  itemClick(index) {
+    this.setState({
+      currentIndex: index
+    })
+
+    const { itemClick } = this.props;
+    itemClick(index)
+  }
 }
+
+// TabControl.propTypes = {
+//   titles: PropTypes.arrayOf(PropTypes.array)
+// }
