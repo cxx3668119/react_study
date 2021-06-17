8b1ded8d15ce6e21e653ed1db3acae8c7f3cabba
cxx3668119 <862704268@qq.com>
2021-06-17 10:16:49 +0800

    day04

--- 02_react-app/src/index.js
+++ 02_react-app/src/index.js
@@ -6,12 +6,8 @@ import { sum } from './utils'
 class App extends React.Component {
   render() {
     return {
-      < div >
-      <h2>
-      </h2>
-      </div >
     }
-}
+  }
 }
 
 ReactDOM.render(<h2>hello,cxx</h2>, document.getElementById('root'))
---
+++ 04_react_component/src/01_组件的定义方式/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+
+
+// export default class App extends Component {
+//   constructor(props) {
+//     super();
+
+//     this.state = {
+//       message: '李银河'
+//     }
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         <span>我是App组件</span>
+//         <h1>{this.state.message}</h1>
+//       </div>
+//     )
+//   }
+// }
+
+export default function App() {
+  return (
+    <div>
+      <span>我是function组件</span>
+      <h2>n h a</h2>
+    </div>
+  )
+}
